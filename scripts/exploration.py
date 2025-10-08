f7e908a532df0035c0ffac77322af9361a1022bf
Matthieu Baric <matthieu.baric@gmail.com>
2025-10-08 16:58:35 +0200

    added fichier de travail fait par simon (Pharma_Ventes_Master_2018)

--- scripts/exploration.py
+++ scripts/exploration.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_excel('output/Pharma_Ventes_Hourly_CLEAN.xlsx')
+df = pd.read_excel('output/Pharma_Ventes_Master_2018plusplus.xlsx')
 
 print(df.head())
 print(df.info())
